bd9794a079a2978f2a0932d48c6ac5ee9a10fa82
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-26 16:15:07 +0200

    Css list + try to update view

--- project-cloud/src/FileList.jsx
+++ project-cloud/src/FileList.jsx
@@ -18,6 +18,32 @@ function FileList() {
     }
   };
 
+  const handleDownload = async (fileName) => {
+    try {
+      const response = await axios.get(`http://localhost:5000/download/${fileName}`, {
+        responseType: "blob",
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", fileName);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (fileName) => {
+    try {
+      await axios.delete(`http://localhost:5000/delete/${fileName}`);
+      fetchFileList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="file-list-container">
       <h2>Liste des fichiers</h2>
@@ -33,8 +59,8 @@ function FileList() {
               <p>Propriétaire : {file.Owner}</p>
               <hr className="file-divider" />
               <div className="button-container">
-                <button className="file-button">Bouton 1</button>
-                <button className="file-button">Bouton 2</button>
+                <button className="file-button" onClick={() => handleDownload(file.Key)}>Télécharger</button>
+                <button className="file-button" onClick={() => handleDelete(file.Key)}>Supprimer</button>
               </div>
             </li>
           ))}
