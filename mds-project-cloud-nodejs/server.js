1edd9503259eb690b62c759bb6646bdae05046d0
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-27 14:20:59 +0200

    Réparation

--- mds-project-cloud-nodejs/server.js
+++ mds-project-cloud-nodejs/server.js
@@ -2,10 +2,10 @@
 require('dotenv').config()
 const express = require('express');
 const AWS = require('aws-sdk');
-const multer  = require('multer');
+const multer = require('multer');
 const fs = require('fs');
 const upload = multer({ dest: 'uploads/' });
-const cors = require('cors'); // Importer le module CORS
+const cors = require('cors');
 
 // Initialiser l'application Express
 const app = express();
@@ -14,7 +14,7 @@ const port = 5000;
 app.use(cors());
 
 // Configure AWS avec votre accessKey, secretAccessKey et région
-const { ACCESS_KEY, SECRET_ACCESS_KEY, REGION } = process.env;
+const { ACCESS_KEY, SECRET_ACCESS_KEY, REGION, AWS_BUCKET_NAME } = process.env;
 
 AWS.config.update({
   accessKeyId: ACCESS_KEY,
@@ -26,13 +26,13 @@ const s3 = new AWS.S3();
 
 // Route par défaut
 app.get('/', (req, res) => {
-  res.send('Hello ca marche bien')
-})
+  res.send('Hello, ça marche bien');
+});
 
 // Route pour lister les éléments du bucket
 app.get('/list', async (req, res) => {
   const params = {
-    Bucket: 'ezytech-mds',
+    Bucket: AWS_BUCKET_NAME,
   };
 
   s3.listObjectsV2(params, (err, data) => {
@@ -60,7 +60,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
   const fileContent = fs.readFileSync(req.file.path);
 
   const params = {
-    Bucket: 'ezytech-mds',
+    Bucket: AWS_BUCKET_NAME,
     Key: req.file.originalname, // Utiliser le nom d'origine du fichier
     Body: fileContent
   };
@@ -69,42 +69,15 @@ app.post('/upload', upload.single('file'), (req, res) => {
     if (err) {
       throw err;
     }
-    res.send({message: "File uploaded successfully. " + data.Location});
+    res.send({ message: "File uploaded successfully. " + data.Location });
   });
 });
 
-
-// // Route pour télécharger un fichier du bucket
-// app.get('/download/:filename', (req, res) => {
-//   const params = {
-//     Bucket: 'ezytech-mds',
-//     Key: req.params.filename
-//   }
-
-//   res.attachment(req.params.filename);
-//   const fileStream = s3.getObject(params).createReadStream();
-//   fileStream.pipe(res);
-// });
-
-// // Route pour supprimer un fichier du bucket
-// app.delete('/delete/:filename', (req, res) => {
-//   const params = {
-//     Bucket: 'ezytech-mds',
-//     Key: req.params.filename
-//   }
-
-//   s3.deleteObject(params, function(err, data) {
-//     if (err) console.log(err, err.stack);  
-//     else     console.log("Bien suppr je suis content");
-//   });
-// });
-
-
-
-app.get("/download/:fileName", (req, res) => {
+// Route pour télécharger un fichier du bucket
+app.get('/download/:fileName', (req, res) => {
   const fileName = req.params.fileName;
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: AWS_BUCKET_NAME,
     Key: fileName,
   };
 
@@ -119,10 +92,11 @@ app.get("/download/:fileName", (req, res) => {
   });
 });
 
-app.delete("/delete/:fileName", (req, res) => {
+// Route pour supprimer un fichier du bucket
+app.delete('/delete/:fileName', (req, res) => {
   const fileName = req.params.fileName;
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: AWS_BUCKET_NAME,
     Key: fileName,
   };
 
@@ -136,8 +110,7 @@ app.delete("/delete/:fileName", (req, res) => {
   });
 });
 
-
 // Démarrer le serveur
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
